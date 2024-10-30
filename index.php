b017dc0a70e755cb06ebbf1d6a1ed26c4c52df55
llfmnn <luisferndz84@gmail.com>
2024-10-30 19:00:43 +0100

    prueba

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina web de Luis</title>
+</head>
+<body>
+    <h1>Luis Fernández Martínez</h1>
+
+    
+    <?php
+        date_default_timezone_set('Europe/Madrid');
+        echo date('H:i:s');
+        ?>
+        <div id="relojCliente"></div>
+        <script src="js/reloj.js"></script>
+
+    <h2>Formulario</h2>
+
+    <form action="insertar.php" method="POST">
+        <label for="nombre">Nombre</label>
+        <input type="text" id="nombre" name="nombre" required>
+        <br>
+        <label for="edad">Edad</label>
+        <input type="number" name="edad" id="edad" min="1" required>
+        <br>
+        <input type="submit" value="Enviar">
+
+
+    </form>
+
+
+
+
+
+</body>
+</html>
---
+++ insertar.php
@@ -0,0 +1,31 @@
+<?php
+    $servername = "localhost"; 
+    $username = "usuario"; //Aquí tienes que poner tu usuario        
+    $password = "usuario"; //Aquí tienes que poner tu pass            
+    $dbname = "prueba"; //Aquí tienes que poner el nombre de tu BD      
+
+    // Crear conexión con la base de datos
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Verificar la conexión
+    if ($conn->connect_error) {
+        die("Error en la conexión: " . $conn->connect_error);
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombre = $_POST['nombre'];
+        $edad = $_POST['edad'];
+        $sql = "INSERT INTO persona (nombre, edad) VALUES ('$nombre', '$edad')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Datos insertados correctamente";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    // Cerrar la conexión a la base de datos
+    $conn->close();
+    //VC6jM8TlxXGlJWP
+?>
+
